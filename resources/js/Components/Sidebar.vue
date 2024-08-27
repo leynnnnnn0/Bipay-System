02c4b3b9cf81cc9fd2232ae9bd79073ab2e158b3
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-08-27 12:24:48 +0800

    added dropdown links for sidebar

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,7 +1,15 @@
 <script setup>
 import SidebarLink from "@/Components/SidebarLink.vue";
 import {usePage} from "@inertiajs/vue3";
+import {ref} from "vue";
 
+const { auth } = usePage().props;
+const isAdmin = auth.user.id === 1;
+
+const showEmployeePageRoutes = ref(false);
+const showEmployeeRoutes = () => {
+    showEmployeePageRoutes.value = !showEmployeePageRoutes.value
+}
 </script>
 
 <template>
@@ -23,7 +31,32 @@ import {usePage} from "@inertiajs/vue3";
         </SidebarLink>
         <SidebarLink :active="usePage().url === '/salary'" location="Salary">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061A1.125 1.125 0 0 1 21 8.689v8.122ZM11.25 16.811c0 .864-.933 1.406-1.683.977l-7.108-4.061a1.125 1.125 0 0 1 0-1.954l7.108-4.061a1.125 1.125 0 0 1 1.683.977v8.122Z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+            </svg>
+        </SidebarLink>
+
+<!--        Admin Links-->
+        <SidebarLink @dropdown-clicked="showEmployeeRoutes" :hasDropDown="true" v-if="isAdmin" :active="usePage().url === '/employees'" location="Employees">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+            </svg>
+        </SidebarLink>
+<!--        Employee Page Options-->
+        <div v-show="showEmployeePageRoutes">
+            <SidebarLink class="h-10 divide-1" v-if="isAdmin" :active="usePage().url === '/add-employee'" location="Add Employee">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M18 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0ZM3 19.235v-.11a6.375 6.375 0 0 1 12.75 0v.109A12.318 12.318 0 0 1 9.374 21c-2.331 0-4.512-.645-6.374-1.766Z" />
+                </svg>
+            </SidebarLink>
+        </div>
+        <SidebarLink v-if="isAdmin" :active="usePage().url === '/schedules'" location="Schedules">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+            </svg>
+        </SidebarLink>
+        <SidebarLink v-if="isAdmin" :active="usePage().url === '/manage-payroll'" location="Manage Payroll">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
             </svg>
         </SidebarLink>
     </div>
--- resources/js/Components/SidebarLink.vue
+++ resources/js/Components/SidebarLink.vue
@@ -13,13 +13,24 @@ defineProps({
     active: {
         type: Boolean,
         default: false,
-    }
+    },
+    hasDropDown: {
+        type: Boolean,
+        default: false
+    },
 })
 </script>
 
 <template>
-    <Link href="#" class="flex items-center gap-3 h-16 px-5 hover:bg-gray-100 transition-colors duration-300 lg:justify-start justify-center" :class="{'bg-gray-300' : active}">
-        <span><slot></slot></span>
-        <span class="font-bold text-lg lg:block hidden">{{ location }}</span>
-    </Link>
+    <div  class="flex items-center gap-3 h-16 px-5 hover:bg-gray-100 transition-colors duration-300" :class="{'bg-gray-300' : active}">
+        <Link href="#" class="w-full flex items-center gap-3 lg:justify-start justify-center">
+            <span><slot></slot></span>
+            <span class="font-bold text-lg lg:block hidden">{{ location }}</span>
+        </Link>
+        <button v-if="hasDropDown" @click="$emit('dropdownClicked')">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+            </svg>
+        </button>
+    </div>
 </template>
